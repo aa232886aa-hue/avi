0ad495c3590a0a4a2912f232c3e3a14f4b22f20f
aa232886aa-hue <aa232886aa@gmail.com>
2026-04-27 12:22:28 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,42 +1,15 @@
-import type {NextConfig} from 'next';
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   output: 'export',
-  typescript: {
-    ignoreBuildErrors: true,
+  images: {
+    unoptimized: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: {
-    unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
-  experimental: {
-    allowedDevOrigins: [
-      '6000-firebase-studio-1776997785130.cluster-l6vkdperq5ebaqo3qy4ksvoqom.cloudworkstations.dev',
-      'localhost:9002'
-    ],
-  },
+  typescript: {
+    ignoreBuildErrors: true,
+  }
 };
 
 export default nextConfig;
